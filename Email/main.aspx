e09ec1fb0c7b94595a8174be94be34b81df5d168
titinho <titusz90@hotmail.hu>
2016-05-04 23:08:50 +0200

    Megy: -> e-mail küldés -> azure adatbázis lekérdezés

---
+++ Email/main.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="main.aspx.cs" Inherits="Email.main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:TextBox ID="TextBox1" runat="server" Height="236px" Width="695px"></asp:TextBox>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ Email/main.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Email
+{
+    public partial class main : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string cs = System.Configuration.ConfigurationManager.ConnectionStrings["my_connection_string"].ConnectionString;
+            //string cs = "Server=tcp:titinho2.database.windows.net,1433;Database=Email;User ID=titinho2@titinho2;Password={your_password_here};Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            using (SqlConnection con = new SqlConnection(cs))
+            try
+            { 
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from People WHERE Performance > 60", con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    TextBox1.Text += reader[0] + "\t";
+                    TextBox1.Text += reader[1] + "\t";
+                    TextBox1.Text += reader[2] + "\t";
+                    TextBox1.Text += reader[3] + "\t";
+                    }
+                    reader.Close();
+
+                    MailMessage mail = new MailMessage(
+                        "titusz90@hotmail.hu",
+                        "titusz90@vipmail.hu",
+                        "Hello Titusz!",
+                        "Üdv Titusz");
+                    SmtpClient client = new SmtpClient("smtp.live.com");
+                    client.Port = 25;
+                    client.Credentials = new System.Net.NetworkCredential("titusz90@hotmail.hu", "titinho90");
+                    client.EnableSsl = true;
+                    client.Send(mail);
+            }
+            catch(Exception ex)
+            {
+                    TextBox1.Text = ex.ToString();
+            }
+        }
+    }
+}
